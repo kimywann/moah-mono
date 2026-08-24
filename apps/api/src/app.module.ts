1068401955f609aab7471763bad66fcb4e140f12
kimywann <kimywan10@gmail.com>
2026-08-24 18:15:51 +0900

    feat(api): Nest에 Prisma 서비스 연결

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { JobPostingModule } from "./jobPosting/jobPosting.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), JobPostingModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    JobPostingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
