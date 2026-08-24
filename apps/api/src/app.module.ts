908de9e671c52e740fd2bd52502deb99c7d2704e
kimywann <kimywan10@gmail.com>
2026-08-24 18:28:43 +0900

    refactor(api): 기본 Nest 예제 코드 제거

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
 import { JobPostingModule } from "./jobPosting/jobPosting.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
@@ -9,9 +8,8 @@ import { PrismaModule } from "./prisma/prisma.module";
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    AuthModule,
     JobPostingModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
