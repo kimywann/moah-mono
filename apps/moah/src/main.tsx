0dc9ed8406fe19f8d15c78c26389445e737b2c22
kimywann <kimywan10@gmail.com>
2026-08-28 12:52:20 +0900

    chore(moah): React Query Devtools 의존성 추가

--- apps/moah/src/main.tsx
+++ apps/moah/src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -18,6 +19,7 @@ createRoot(rootElement).render(
       <AuthProvider>
         <App />
       </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
