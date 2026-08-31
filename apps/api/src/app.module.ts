a7aa7c6151a52db991b2750ef3de905f2740588b
kimywann <kimywan10@gmail.com>
2026-08-31 22:45:24 +0900

    feat(api): 헬스체크 API 추가

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ApplicationsModule } from "./applications/applications.module";
 import { AuthModule } from "./auth/auth.module";
+import { HealthModule } from "./health/health.module";
 import { JobPostingModule } from "./jobPosting/jobPosting.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
@@ -12,6 +13,7 @@ import { PrismaModule } from "./prisma/prisma.module";
     ApplicationsModule,
     AuthModule,
     JobPostingModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
